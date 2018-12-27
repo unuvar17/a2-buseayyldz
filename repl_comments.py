9dc58b4a341e840133bf3c0dae86b20ab590a457
Buse <buseayyildiz55@gmail.com>
2018-12-27 04:39:12 +0300

    assignment

--- repl_comments.py
+++ repl_comments.py
@@ -1,4 +1,20 @@
-print("Enter your comment:")
-x = input()
+from hashlib import sha256
 
+
+def create_hash(password):
+    pw_bytestring = password.encode()
+    return sha256(pw_bytestring).hexdigest()
+ 
+
+password = "3a7bd3e2360a3d29eea436fcfb7e44c735d117c42d1c1835420b6b9942dd4f1b"
+
+password_input = input ("Enter Password: ")
+
+password_input_hash = create_hash(password_input)
+       
+while password == password_input_hash:
+           print ("Enter your comment")
+           comment = input()
+if password != password_input_hash:
+           print ("I'm sorry I can’t let you do that.")
 
